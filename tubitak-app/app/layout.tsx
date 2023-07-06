59866bffa9827c5bc63153b7269ab66a5788f8e7
Ahmet Tuna POTUR <tpotur@gmail.com>
2023-07-06 22:42:42 +0300

    some updates

--- tubitak-app/app/layout.tsx
+++ tubitak-app/app/layout.tsx
@@ -1,21 +1,24 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import StyledComponentsRegistry from "@/lib/registry";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Başarılarım',
-  description: 'Başarılarını tarihe not düş.',
-}
+  title: "Başarılarım",
+  description: "Başarılarını tarihe not düş.",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="tr">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+      </body>
     </html>
-  )
+  );
 }
