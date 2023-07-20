9176c7b5dad40717e414a7c3ed666113acf682ff
Ahmet Tuna POTUR <tpotur@gmail.com>
2023-07-20 22:08:22 +0300

    tailwind

--- tubitak-app/components/success-card/index.tsx
+++ tubitak-app/components/success-card/index.tsx
@@ -3,10 +3,12 @@ import ISuccess from '@/interfaces/success';
 
 const SuccessCard = (probs: ISuccess) => {
   return (
-    <li key={probs.id}>
-      <h5>{probs.title}</h5>
-      <p>{probs.description}</p>
-      <div>{probs.date}</div>
+    <li className="rounded-md bg-slate-200 p-3 shadow-md" key={probs.id}>
+      <div className="mb-2 flex items-center justify-between">
+        <h5 className="text-lg font-medium uppercase">{probs.title}</h5>
+        <div className="text-sm font-light">{probs.date}</div>
+      </div>
+      <p className="px-1">{probs.description}</p>
     </li>
   );
 };
--- tubitak-app/components/success-list/index.tsx
+++ tubitak-app/components/success-list/index.tsx
@@ -4,7 +4,7 @@ import ISuccessList from '@/interfaces/success-list';
 
 const SuccessCardList = ({ success_list }: { success_list: ISuccessList }) => {
   return (
-    <ul className='flex flex-col space-y-6 py-6 px-1'>
+    <ul className="mx-2 my-4 flex flex-col space-y-6">
       {success_list.map((success) => (
         <SuccessCard key={success.id} {...success} />
       ))}
